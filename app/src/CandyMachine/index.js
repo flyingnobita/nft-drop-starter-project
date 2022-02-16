08f5d3f87326cc5cf9d6de6e112e257910d5407e
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2022-02-16 15:14:49 +0800

    Finish Section 2

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -92,7 +92,7 @@ const CandyMachine = ({ walletAddress }) => {
     const userTokenAccountAddress = (
       await getAtaForMint(mint.publicKey, walletAddress.publicKey)
     )[0];
-  
+  ``
     const userPayingAccountAddress = candyMachine.state.tokenMint
       ? (await getAtaForMint(candyMachine.state.tokenMint, walletAddress.publicKey))[0]
       : walletAddress.publicKey;
